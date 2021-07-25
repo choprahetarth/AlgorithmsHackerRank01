17711ba9a656bede768d10dd570ce8e15221547d
hetarth <choprahetarth@gmail.com>
2021-07-25 23:44:52 +0530

    adding another question

---
+++ SQL Hackerrank/weatherStation6.sql
@@ -0,0 +1,11 @@
+''' link-> https://www.hackerrank.com/challenges/weather-observation-station-6/problem?h_r=next-challenge&h_v=zen'''
+SELECT CITY FROM STATION
+WHERE CITY LIKE "a%";
+SELECT CITY FROM STATION
+WHERE CITY LIKE "e%";
+SELECT CITY FROM STATION
+WHERE CITY LIKE "i%";
+SELECT CITY FROM STATION
+WHERE CITY LIKE "o%";
+SELECT CITY FROM STATION
+WHERE CITY LIKE "u%";
